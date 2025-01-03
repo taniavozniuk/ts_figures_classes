716059c0c778e4dfd8ea7d202cbb49e480709c80
Tetiana Vozniuk <taniavoznyk19655@gmail.com>
2025-01-03 12:02:25 +0200

    add task solution

--- src/figuresClasses.ts
+++ src/figuresClasses.ts
@@ -1,10 +1,50 @@
-export interface Figure {}
+export interface Figure {
+  shape: string;
+  color: string;
+  getArea(): number;
+}
 
-export class Triangle implements Figure {}
+export class Triangle implements Figure {
+  shape = 'triangle';
+  color = 'red';
 
-export class Circle implements Figure {}
+  constructor(
+    public a: number,
+    public b: number,
+    public c: number,
+  ) {}
 
-export class Rectangle implements Figure {}
+  getArea(): number {
+    const p = (this.a + this.b + this.c) / 2; // напівпериметр
+
+    return Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)); // формула
+  }
+}
+
+export class Circle implements Figure {
+  shape = 'circle';
+  color = 'green';
+
+  constructor(public radius: number) {}
+
+  getArea(): number {
+    return Math.PI * this.radius ** 2;
+  }
+}
+
+export class Rectangle implements Figure {
+  shape = 'rectangle';
+  color = 'blue';
+
+  constructor(
+    public width: number,
+    public height: number,
+  ) {}
+
+  getArea(): number {
+    return this.width * this.height;
+  }
+}
 
 export function getInfo(figure): string {
   return typeof figure;
